e0e838bd1efa52e39de3a27ae512b7cb99f87b63
Charles <wtm0217@gmail.com>
2018-11-14 17:10:46 +0800

    Add files via upload

--- 30bins/HTsensor_simulation_generateLib.py
+++ 30bins/HTsensor_simulation_generateLib.py
@@ -138,6 +138,8 @@ def cytometry_sorting(pop_DF,exp_DF,Bin_bou_con,total_cells):
     sort_Dic={}
     exp_Lst=exp_DF.columns.tolist()
     bins_Lst=exp_DF.index.tolist()
+    print (Bin_bou_con.index.tolist())
+    print (bins_Lst)
     if Bin_bou_con.index.tolist()!=bins_Lst:
         logging.error('Contradictory bin setting!')
         sys.exit(-1)
@@ -209,9 +211,7 @@ def htsensorsimu_generate_main(args):
     # Sub configure file to define the experimental setting
     exp_DF=pd.read_csv(filepath_or_buffer=args.exp_con,sep='\t',index_col='Bin')
     exp_Lst=exp_DF.columns.tolist()
-    exp_Lst.sort()
     bins_Lst=exp_DF.index.tolist()
-    bins_Lst.sort()
     #
     # Sub configure file to define the boundaries between bins for each sorting experiemnt
     # columns = ligand1R1 ligand1R2 ligand2R1 ligand2R2 ... (experiment condition)
@@ -224,6 +224,7 @@ def htsensorsimu_generate_main(args):
             temp=Bin_bou_DF.loc[bins,exp].split(',')
             Bin_bou_Dic[exp][bins]=[float(temp[0]),float(temp[1])]
     Bin_bou_DF=pd.DataFrame(Bin_bou_Dic)
+    Bin_bou_DF=Bin_bou_DF.reindex(bins_Lst)
     # total reads of every sequencing library
     total_reads=args.total_reads
     # total cells of sorting experiment
